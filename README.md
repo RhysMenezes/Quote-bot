c74a521d92fba3ae002565c339f4ac34c9ac5fe5
Rhys Menezes <rhysmenezes@gmail.com>
2018-08-16 21:44:39 +0930

    added unique comments, random quotes and time
    Made it so we only store unique quotes in the db. Added a random function to print a random quote. Made it so it keeps track of the time the quote was added

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Quote-bot
 A discord bot that can store quotes/messages from users in a database which can be called at any time.
 
+## Requirements
+```
+Python 3.5+
+discord.py
+```
+
 ## How to run
+1. Install the discord.py package.
 ```
 pip install discord.py
 ```
-Add your token key where it says [token] and run:
+2. Create a bot application [here](https://discordapp.com/developers/applications/) and get a discord token/secret key.
+
+3. Add your token/secret key where it says [token] at the bottom of the code and run:
 ```
 python quotebot.py
 ```
 ## Commands
-!quote [user] [message] - stores a quote by the user  
-!getquote [user] - gets a users quote  
+!quote @[user] [message] - stores a quote by the user  
+!getquote @[user] - gets a users quote  
 !quotehelp - shows a list of commands and syntax
+!random - gets a random quote from a random user
+
+##FAQ
+* The program has checks to stop people adding the same quote to a given person
+
+* Check your python version if you have async problems
--- quotebot.py
+++ quotebot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import time
+import datetime
 import hashlib
 import sqlite3
 
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='!')
 #check if database is made and load it
 db = sqlite3.connect('quotes.db')
 cursor = db.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS quotes(hash TEXT primary key, user TEXT, message TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS quotes(hash TEXT primary key, user TEXT, message TEXT, date_added TEXT)')
 print("Loaded database")
 
 db.commit()
@@ -28,14 +28,30 @@ async def ping(ctx):
     await bot.say("pong")
     print ("ping sent")
 
-
+#help menu
 @bot.command()
 async def quotehelp():
     embed = discord.Embed(name="help")
     embed.set_author(name="Quotebot commands:")
-    embed.add_field(name="To quote:", value="!quote [user] [message]", inline=False)
-    embed.add_field(name="To display", value="!getquote [user]", inline=False)
+    embed.add_field(name="To quote:", value="!quote @[user] [message]", inline=False)
+    embed.add_field(name="To display", value="!getquote @[user]", inline=False)
+    embed.add_field(name="Random quote from a random user", value="!random", inline=False)
     await bot.say(embed=embed)
+
+#print random quote
+@bot.command()
+async def random():
+
+    cursor.execute("SELECT user,message,date_added FROM quotes ORDER BY RANDOM() LIMIT 1")
+    query = cursor.fetchone()
+
+    #log
+    print(query[0]+": \""+query[1]+"\" printed to the screen "+str(query[2]))
+
+    #embeds the output
+    style = discord.Embed(name="responding quote", description="- "+str(query[0])+" "+str(query[2]))
+    style.set_author(name=str(query[1]))
+    await bot.say(embed=style)
 
 
 @bot.command()
@@ -51,41 +67,56 @@ async def quote(*, message: str):
 
     #join the message back together
     text = " ".join(temp)
+    
+    if user[1]!='@':
+        await bot.say("Use ```@[user] [message]``` to quote a person")
+        return
 
-    uniqueID=hash(user+message)
+    uniqueID = hash(user+message)
+
+    #date and time of the message
+    time = datetime.datetime.now()
+    formatted_time = str(time.strftime("%d-%m-%Y %H:%M"))
+
+    #find if message is in the db already
+    cursor.execute("SELECT count(*) FROM quotes WHERE hash = ?",(uniqueID,))
+    find = cursor.fetchone()[0]
+
+    if find>0:
+        return
 
     #insert into database
-    cursor.execute("INSERT INTO quotes VALUES(?,?,?)",(uniqueID,user,text))
-    await bot.say("quote successfully added")
+    cursor.execute("INSERT INTO quotes VALUES(?,?,?,?)",(uniqueID,user,text,formatted_time))
+    await bot.say("Quote successfully added")
 
     db.commit()
 
     #number of words in the database
-    rows = cursor.execute('SELECT * from quotes')
+    rows = cursor.execute("SELECT * from quotes")
 
     #log to terminal
-    print(len(rows.fetchall())+". added - "+user+": \""+text+"\" to database")
+    print(str(len(rows.fetchall()))+". added - "+str(user)+": \""+str(text)+"\" to database at "+formatted_time)
 
 
 @bot.command()
 async def getquote(message: str):
     
-    #sanitise input
-    user=(message,)
+    #sanitise name
+    user = (message,)
 
     try:
         #query random quote from user
-        cursor.execute('SELECT message FROM quotes WHERE user=(?) ORDER BY RANDOM() LIMIT 1',user)
+        cursor.execute("SELECT message,date_added FROM quotes WHERE user=(?) ORDER BY RANDOM() LIMIT 1",user)
         query = cursor.fetchone()
 
-        #adds quotes
-        output="\""+" ".join(query)+"\""
+        #adds quotes to message
+        output = "\""+str(query[0])+"\""
 
         #log
-        print(message+": \""+output+"\" printed to the screen")
+        print(message+": \""+output+"\" printed to the screen "+str(query[1]))
 
         #embeds the output to make it pretty
-        style = discord.Embed(name="responding quote", description="- "+message)
+        style = discord.Embed(name="responding quote", description="- "+message+" "+str(query[1]))
         style.set_author(name=output)
         await bot.say(embed=style)
 
@@ -96,4 +127,4 @@ async def getquote(message: str):
     db.commit()    
 
 
-bot.run("token")
+bot.run("[token]")
